99f6249bffe328caed057680eaf382820edca7f3
arhipovana2001 <arhipovana2001@mail.ru>
2019-11-03 15:00:13 +0700

    ready

--- main.py
+++ main.py
@@ -1,28 +1,58 @@
-#number = int(input('Введите количество должников: '))
-#while number <= 0:
- #   print('Ошибка. Число не может быть меньшим или равным нулю.')
-  #  number = int(input('Введите количество должников: '))
-#print(number)
-#name = str(input('Введите список должников (обратите внимание,
-# что список должен заканчиваться пробелом в конце): '))
-number = 9
-name = """Мясников Никифор Иринеевич Трофимова Альбина Феликсовна """
-k = name.count(' ')
-print(k)
-name_all = ' '
-words = 0
+print('--------------------------------------------------------------------------')
+print('        ФИ         |     долг     |     платеж     |    остаток долга    |')
+print('                   |   тыс. руб.  |    тыс. руб.   |      тыс. руб.      |')
+print('--------------------------------------------------------------------------')
 
-for i in range(1,k): #4 words
-    n = name.find(' ') #нахожу индекс пробела
-    if n == -1:
-        name_all = name[:n+1]  # выписываю слово
-        name = name[n + 1:]  # отбрасываю слово
-        print(name_all, end=' ')
-        words += 1
-    else:
-        name_all = name[:n]  # выписываю слово
-        name = name[n + 1:]  # отбрасываю слово
-        print(name_all, end=' ')
-        words += 1
-    if words == 3:
-        print()
+registry = '''Мясников Никифор 57689/Трофимова Альбина 342525/Макаров Артем 444/Ревтова Лидия 3893/Зайцева Анастасия 829/'''
+
+k = 0
+debt = 0
+name = ''
+balance_owed = 0
+first_column = 19
+payment = 0
+
+number = registry.count('/')
+for i in range (1, number + 1):
+    index = registry.find('/')
+    name = registry[:index]
+    registry = registry[index + 1:]
+    k = name.count(' ')
+    name_all = ''
+    while k + 1 > 0:
+        name = name
+        n1 = name[:name.find(' ') + 1]
+        #if n1.isdigit():
+         #   debt = int(n1)
+        name_all = name_all + n1
+        name = name[name.find(' ') + 1:]
+        k -= 1
+    symbol = first_column - len(name_all)
+    print(name_all + ' ' * symbol, end = '')
+    print('|', end = '')
+    payment = str(ord(name_all[0]))
+    debt = payment[::-1]
+    payment = int(payment)
+    debt = int(debt)
+    print(format(debt, "14.0f"), end='')
+    print('|', end='')
+    print(format(payment, "16.0f"), end='')
+    print('|', end='')
+    balance_owed = debt - payment
+    print(format(balance_owed, "21.0f"), end='')
+    print('|')
+
+
+    #debt = int(input('Введите долг: '))
+    #while debt < 0:
+     #   print('Ошибка. Число не может быть меньше нуля.')
+      #  debt = int(input('Введите долг: '))
+
+    #payment = int(input('Введите платеж: '))
+    #while payment < 0:
+     #   print('Ошибка. Число не может быть меньше нуля.')
+      #  payment = int(input('Введите платеж: '))
+
+    #balance_owed = debt - payment
+    #print('Остаток долга =', balance_owed)
+print('--------------------------------------------------------------------------')
